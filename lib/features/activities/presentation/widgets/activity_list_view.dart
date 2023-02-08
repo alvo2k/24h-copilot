87282548ded02b39321c53a053f4b56b155ba7ee
alvo <voenkovalex@gmail.com>
2023-02-09 02:00:44 +0300

    [Activity] Hopefully final iteration on list view

--- lib/features/activities/presentation/widgets/activity_list_view.dart
+++ lib/features/activities/presentation/widgets/activity_list_view.dart
@@ -21,59 +21,69 @@ class ActivityListView extends StatelessWidget {
         },
         (loading) => const Center(child: CircularProgressIndicator()),
         (loaded) {
-          return ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            reverse: true,
+          return SingleChildScrollView(
             controller: controller,
+            reverse: true,
             keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            itemCount: loaded.days.length,
-            itemBuilder: (context, index) {
-              // if (loaded.days.length == 1 &&
-              //     loaded.days[0].activitiesInThisDay.isEmpty) {
-              //   // app launches for the first time
-              //   return const Text('First launch');
-              // }
-              final day = loaded.days[index];
-              if (day.activitiesInThisDay.isEmpty) {
-                return const SizedBox.shrink();
-              }
-              if (index == loaded.days.length - 1 &&
-                  day.activitiesInThisDay.isNotEmpty) {
-                return Column(children: [
-                  // adds loading indicator at top of the list. Empty activitiesDay means all data was loaded and no indicator needed 
-                  const CircularProgressIndicator(),
-                  DateFormat(day.date),
-                  ActivityDayWidget(day),
-                ]);
-              }
-              if (index == 0) {
-                // if this is today, print icon at the end
+            physics: const BouncingScrollPhysics(),
+            child: ListView.builder(
+              shrinkWrap: true,
+              reverse: true,
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              physics: const BouncingScrollPhysics(),
+              itemCount: loaded.days.length,
+              itemBuilder: (context, index) {
+                // if (loaded.days.length == 1 &&
+                //     loaded.days[0].activitiesInThisDay.isEmpty) {
+                //   // app launches for the first time
+                //   return const Text('First launch');
+                // }
+                final day = loaded.days[index];
+                if (day.activitiesInThisDay.isEmpty) {
+                  return const SizedBox.shrink();
+                }
+                if (index == 0) {
+                  Future(() {
+                    // calls loadMoreDays() witch loads prev. day
+                    controller.position.notifyListeners();
+                  });
+                  // if this is today, print icon at the end
+                  return Column(
+                    children: [
+                      DateFormat(day.date),
+                      ActivityDayWidget(day),
+                      Row(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 29.0, vertical: 16),
+                            child: Icon(
+                              Icons.access_time,
+                              color: Colors.black38,
+                            ),
+                          ),
+                        ],
+                      ),                      
+                    ],
+                  );
+                }
+                if (index == loaded.days.length - 1 &&
+                    day.activitiesInThisDay.isNotEmpty) {
+                  return Column(children: [
+                    // adds loading indicator at top of the list. Empty ActivitiesDay means all data was loaded and no indicator needed
+                    const CircularProgressIndicator(),
+                    DateFormat(day.date),
+                    ActivityDayWidget(day),
+                  ]);
+                }
                 return Column(
                   children: [
                     DateFormat(day.date),
                     ActivityDayWidget(day),
-                    Row(
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 29.0, vertical: 16),
-                          child: Icon(
-                            Icons.access_time,
-                            color: Colors.black38,
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 );
-              }
-              return Column(
-                children: [
-                  DateFormat(day.date),
-                  ActivityDayWidget(day),
-                ],
-              );
-            },
+              },
+            ),
           );
         },
         (failure) => Center(child: Text(failure.message)),

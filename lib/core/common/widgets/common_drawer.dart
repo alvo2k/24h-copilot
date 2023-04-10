8cac731cd452e743db08a19e14e7f1f4e7fb298b
alvo <voenkovalex@gmail.com>
2023-04-10 18:18:56 +0300

    Fix abandoned import.

--- lib/core/common/widgets/common_drawer.dart
+++ lib/core/common/widgets/common_drawer.dart
@@ -1,4 +1,3 @@
-import '../../../features/firebase/presentation/widgets/sync_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';

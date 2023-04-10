80c95a8f0749d6b3b46ce200f2c173d90334ea5a
alvo <voenkovalex@gmail.com>
2023-04-10 14:28:55 +0300

    [Fix] database is locked error

--- lib/features/firebase/presentation/bloc/auth_bloc.dart
+++ lib/features/firebase/presentation/bloc/auth_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:copilot/core/error/return_types.dart';
-import 'package:copilot/features/firebase/domain/usecases/auth_usecase.dart';
+import '../../../../core/error/return_types.dart';
+import '../../domain/usecases/auth_usecase.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sealed_flutter_bloc/sealed_flutter_bloc.dart';
@@ -9,7 +9,6 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(AuthUsecase usecase) : super(AuthState.initial()) {
-    usecase(AuthParams());
     on<AuthEvent>((event, emit) async {
       await event.join((register) async {
         emit(AuthState.loading());
@@ -42,7 +41,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         final result = await usecase(AuthParams());
         result.fold((l) {
           if (l is UnsupportedPlatformFailure) {
-            emit(AuthState.failure('Authentication and synchronization is not supported on this platform'));
+            emit(AuthState.failure(
+                'Authentication and synchronization is not supported on this platform'));
           }
         }, (r) {
           final user = usecase.getUser();

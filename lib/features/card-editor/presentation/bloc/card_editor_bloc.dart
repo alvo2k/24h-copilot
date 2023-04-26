ae9e401318293dfef40b1ce9e0ebabdeaf5c6502
alvo <voenkovalex@gmail.com>
2023-04-26 20:29:06 +0300

    Card-editor: bloc

---
+++ lib/features/card-editor/presentation/bloc/card_editor_bloc.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/common/activity_settings.dart';
+import '../../domain/usecases/load_activities_settings_usecase.dart';
+import '../../domain/usecases/update_activity_settings_usecase.dart';
+
+part 'card_editor_event.dart';
+part 'card_editor_state.dart';
+
+class CardEditorBloc extends Bloc<CardEditorEvent, CardEditorState> {
+  final LoadActivitiesSettingsUsecase loadUsecase;
+  final UpdateActivitySettingsUsecase updateUsecase;
+  CardEditorBloc(this.loadUsecase, this.updateUsecase)
+      : super(CardEditorStateInitial()) {
+    on<CardEditorEvent>((event, emit) async {
+      if (event is LoadActivitiesSettings) {
+        emit(CardEditorStateLoading());
+        loadUsecase(LoadActivitiesSettingsParams()).then(
+          (result) => result.fold(
+            (l) => emit(CardEditorStateFailure(l.prop['message'])),
+            (r) => emit(CardEditorStateLoaded(r)),
+          ),
+        );
+      } else if (event is UpdateActivitiesSettings) {
+        emit(CardEditorStateLoading());
+        updateUsecase(UpdateActivitySettingsParams(
+          activityName: event.activityName,
+          newActivityName: event.newActivityName,
+          newColor: event.newColor,
+          newGoal: event.newGoal,
+          tags: event.tags,
+        )).then(
+          (result) => result.fold(
+            (l) => emit(CardEditorStateFailure(l.prop['message'])),
+            (r) => emit(CardEditorStateUpdated(r)),
+          ),
+        );
+      }
+    });
+  }
+}
---
+++ lib/features/card-editor/presentation/bloc/card_editor_state.dart
@@ -0,0 +1,33 @@
+part of 'card_editor_bloc.dart';
+
+abstract class CardEditorState extends Equatable {
+  const CardEditorState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CardEditorStateInitial extends CardEditorState {}
+
+class CardEditorStateLoading extends CardEditorState {}
+
+class CardEditorStateLoaded extends CardEditorState {
+  final List<ActivitySettings> activitiesSettings;
+
+  const CardEditorStateLoaded(this.activitiesSettings);
+
+  @override
+  List<Object> get props => [activitiesSettings];
+}
+
+class CardEditorStateFailure extends CardEditorState {
+  final String message;
+
+  const CardEditorStateFailure(this.message);
+}
+
+class CardEditorStateUpdated extends CardEditorState {
+  final ActivitySettings settings;
+
+  const CardEditorStateUpdated(this.settings);
+}

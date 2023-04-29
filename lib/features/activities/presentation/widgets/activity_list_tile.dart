8f5fe3788880c4ba93f2c09cb9bd543d729090f4
alvo <voenkovalex@gmail.com>
2023-04-29 11:00:56 +0300

    Fix tags layout

--- lib/features/activities/presentation/widgets/activity_list_tile.dart
+++ lib/features/activities/presentation/widgets/activity_list_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -159,13 +161,22 @@ class ActivityListTile extends StatelessWidget {
                       ],
                     ),
                     // tags
-                    SizedBox(
-                      width: 120,
-                      height: 35,
-                      child: ListView(
-                        reverse: true,
-                        scrollDirection: Axis.horizontal,
-                        children: buildTags(activity.tags ?? []),
+                    Expanded(
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          return SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: max(constraints.maxWidth, 150),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: buildTags(activity.tags ?? []),
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ],

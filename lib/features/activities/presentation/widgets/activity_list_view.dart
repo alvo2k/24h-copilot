0be8a1b6b4826bacd37080308e417026ae75dbd9
alvo <voenkovalex@gmail.com>
2023-09-20 15:47:43 +0300

    Change: remove unnecessary widget

--- lib/features/activities/presentation/widgets/activity_list_view.dart
+++ lib/features/activities/presentation/widgets/activity_list_view.dart
@@ -25,70 +25,66 @@ class ActivityListView extends StatelessWidget {
         (loading) => const Center(child: CircularProgressIndicator()),
         (loaded) {
           final bloc = BlocProvider.of<ActivitiesBloc>(context);
-          return SingleChildScrollView(
-            controller: controller,
-            reverse: true,
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            physics: const BouncingScrollPhysics(),
-            child: Obx(
-              () {
-                if (bloc.loadedActivities.length == 1 &&
-                    bloc.loadedActivities[0].activitiesInThisDay.isEmpty) {
-                  return const EmptyActivitiesIllustration();
-                }
-                return ListView.builder(
-                  shrinkWrap: true,
-                  reverse: true,
-                  keyboardDismissBehavior:
-                      ScrollViewKeyboardDismissBehavior.onDrag,
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: bloc.loadedActivities.length,
-                  itemBuilder: (context, index) {
-                    final day = bloc.loadedActivities[index];
-                    if (day.activitiesInThisDay.isEmpty) {
-                      return const SizedBox.shrink();
-                    }
-                    if (index == 0) {
-                      Future(() {
-                        // calls loadMoreDays() witch loads prev. day
-                        controller.position.notifyListeners();
-                      });
-                      // if this is today, print icon at the end
-                      return Column(
-                        children: [
-                          ActivityDayDate(day.date),
-                          ActivityDayWidget(day),
-                          const Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 29.0, vertical: 16),
-                                child: Icon(Icons.access_time),
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    }
-                    if (index == loaded.days.length - 1 &&
-                        day.activitiesInThisDay.isNotEmpty) {
-                      return Column(children: [
-                        // adds loading indicator at top of the list. Empty ActivitiesDay means all data was loaded and no indicator needed
-                        const CircularProgressIndicator(),
-                        ActivityDayDate(day.date),
-                        ActivityDayWidget(day),
-                      ]);
-                    }
+          return Obx(
+            () {
+              if (bloc.loadedActivities.length == 1 &&
+                  bloc.loadedActivities[0].activitiesInThisDay.isEmpty) {
+                return const EmptyActivitiesIllustration();
+              }
+              return ListView.builder(
+                key: const PageStorageKey('activityListView'),
+                controller: controller,
+                shrinkWrap: true,
+                reverse: true,
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                physics: const BouncingScrollPhysics(),
+                itemCount: bloc.loadedActivities.length,
+                itemBuilder: (context, index) {
+                  final day = bloc.loadedActivities[index];
+                  if (day.activitiesInThisDay.isEmpty) {
+                    return const SizedBox.shrink();
+                  }
+                  if (index == 0) {
+                    Future(() {
+                      // calls loadMoreDays() witch loads prev. day
+                      controller.position.notifyListeners();
+                    });
+                    // if this is today, print icon at the end
                     return Column(
                       children: [
                         ActivityDayDate(day.date),
                         ActivityDayWidget(day),
+                        const Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 29.0, vertical: 16),
+                              child: Icon(Icons.access_time),
+                            ),
+                          ],
+                        ),
                       ],
                     );
-                  },
-                );
-              },
-            ),
+                  }
+                  if (index == loaded.days.length - 1 &&
+                      day.activitiesInThisDay.isNotEmpty) {
+                    return Column(children: [
+                      // adds loading indicator at top of the list. Empty ActivitiesDay means all data was loaded and no indicator needed
+                      const CircularProgressIndicator(),
+                      ActivityDayDate(day.date),
+                      ActivityDayWidget(day),
+                    ]);
+                  }
+                  return Column(
+                    children: [
+                      ActivityDayDate(day.date),
+                      ActivityDayWidget(day),
+                    ],
+                  );
+                },
+              );
+            },
           );
         },
         (failure) => Center(child: Text(failure.message)),

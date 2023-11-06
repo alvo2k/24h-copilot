4110702780c5ff8b4561d08659a84e021c0747b9
alvo <voenkovalex@gmail.com>
2023-11-06 10:22:09 +0300

    add: scroll bar to recommended activities

--- lib/features/activities/presentation/widgets/recommended_activities.dart
+++ lib/features/activities/presentation/widgets/recommended_activities.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -5,9 +7,11 @@ import '../bloc/activities_bloc.dart';
 import 'recomend_activity_chip.dart';
 
 class RecommendedActivities extends StatelessWidget {
-  const RecommendedActivities({super.key, required this.child});
+  RecommendedActivities({super.key, required this.child});
 
   final Widget child;
+
+  final _controller = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +30,24 @@ class RecommendedActivities extends StatelessWidget {
                 }
                 return SizedBox(
                   height: 40,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: state.pageState.recommendedActivities.length,
-                    itemBuilder: (context, index) {
-                      final activity =
-                          state.pageState.recommendedActivities[index];
-                      return RecomendActivityChip(
-                        name: activity.name,
-                        color: activity.color,
-                        isFirstRecomended: index == 0,
-                      );
-                    },
+                  width: 450,
+                  child: Scrollbar(
+                    thickness: Platform.isAndroid || Platform.isIOS ? 0 : 8,
+                    controller: _controller,
+                    child: ListView.builder(
+                      controller: _controller,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: state.pageState.recommendedActivities.length,
+                      itemBuilder: (context, index) {
+                        final activity =
+                            state.pageState.recommendedActivities[index];
+                        return RecomendActivityChip(
+                          name: activity.name,
+                          color: activity.color,
+                          isFirstRecomended: index == 0,
+                        );
+                      },
+                    ),
                   ),
                 );
               },
